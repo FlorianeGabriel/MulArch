1a4ce4310f656bea62939fe484b1ae2ed8807b03
foocil <julien.vonfelten@epfl.ch>
2018-11-05 09:02:25 -0800

    Update make file and files

--- assignment2/execute.sh
+++ assignment2/execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --workdir /home/vonfelte
+#SBATCH --workdir /home/fgabriel
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16
@@ -7,6 +7,31 @@
 
 echo STARTING AT `date`
 
-./assignment2 1 10 1 output.csv
+./assignment2 1 10000 100 output.csv
+./assignment2 2 10000 100 output.csv
+./assignment2 4 10000 100 output.csv
+./assignment2 8 10000 100 output.csv
+./assignment2 16 10000 100 output.csv
+
+./assignment2FirstTry 1 10000 100 output.csv
+./assignment2FirstTry 2 10000 100 output.csv
+./assignment2FirstTry 4 10000 100 output.csv
+./assignment2FirstTry 8 10000 100 output.csv
+
+./assignment2FirstTryNoIF 1 10000 100 output.csv
+./assignment2FirstTryNoIF 2 10000 100 output.csv
+./assignment2FirstTryNoIF 4 10000 100 output.csv
+./assignment2FirstTryNoIF 8 10000 100 output.csv
+
+./assignment2Tiles1 1 10000 100 output.csv
+./assignment2Tiles1 2 10000 100 output.csv
+./assignment2Tiles1 4 10000 100 output.csv
+./assignment2Tiles1 8 10000 100 output.csv
+
+./assignment2TilesBest 1 10000 100 output.csv
+./assignment2TilesBest 2 10000 100 output.csv
+./assignment2TilesBest 4 10000 100 output.csv
+./assignment2TilesBest 8 10000 100 output.csv
+
 
 echo FINISHED at `date`
